593832307cbd56fc1218a2fa190051dae6297b84
Aniket12215523 <115388792+Aniket12215523@users.noreply.github.com>
2022-10-30 23:51:45 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+def my_func():
+     print('hello world')
+    
+my_func()
+my_func()
+my_func()
+my_func()
+my_func()
+
+# data into functions as arguments
+
+def my_func(fName, lname):
+    print('My name is ' + fName + lname)
+    
+my_func("John", "Cena")
