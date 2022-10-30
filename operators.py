babcfd219263fd63d9b04b5c204f792ba4a3e7bb
Aniket12215523 <115388792+Aniket12215523@users.noreply.github.com>
2022-10-30 23:41:25 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,28 @@
+#airthimetic operators
+x=5
+y=2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x**y)
+print(x/y)
+print(x%y)
+print(x//y)
+#comparison operator
+x=2
+y=3
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x>=5)
+#logical operator
+x=3
+print(x<5 and x<6)
+print(x<1 or x<7)
+print(not(x>2 and x>4))
+#identity operators
+a=[10,20]
+b=[10,20]
+c=a
+print(a is c)
+print(b is c)
